6c9d67ccd6dcf07d8677b2f16948123abfe59c46
unknown <hayounn.etd@gmail.com>
2022-09-22 15:19:36 +0100

    Modify updating methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :post, class_name: 'Post'
   belongs_to :author, class_name: 'User', foreign_key: :author_id
+  after_save :update_comments_counter
   
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :likes, foreign_key: 'post_id'
   has_many :comments, foreign_key: 'post_id'
-  after_save :update_post_counter
+  after_save :update_posts_counter
 
-  scope :update_post_counter, ->(user_id) { User.find(user_id).update(post_counter: User.find(user_id).posts.count) }
-  scope :five_recent_comment, ->(post) { Comment.where(post_id: post).order(created_at: :desc).limit(3) }
-
-
+  scope :five_recent_comments, ->(post) { Comment.where(post_id: post).order(created_at: :desc).limit(3) }
+  
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+  
 end
