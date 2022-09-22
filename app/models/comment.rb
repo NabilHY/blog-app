a51fd678cd93dd1f4ea5aac2d2321784b660b642
unknown <hayounn.etd@gmail.com>
2022-09-22 14:50:03 +0100

    User method for returning posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :user
+  belongs_to :post, class_name: 'Post'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
     has_many :comments, foreign_key: 'author_id'
     has_many :likes, foreign_key: 'author_id'
     has_many :posts, foreign_key: 'author_id'
+    
+    scope :recent_posts, ->(user_id) { Post.where(author_id: user_id).order(created_at: :desc).limit(3) }
+    
 
 end
