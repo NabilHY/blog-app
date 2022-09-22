0c7e42b92b960372a4e17bfdb17eef543180413b
unknown <hayounn.etd@gmail.com>
2022-09-22 11:13:53 +0100

    Restructure db

--- db/migrate/20220921104757_add_post_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddPostToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_column :comments, :post, :integer
-    add_index :comments, :post
-  end
-end
--- db/migrate/20220921104937_add_user_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddUserToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :user, :integer
-    add_index :posts, :user
-  end
-end
