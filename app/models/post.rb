a1146456144cbbf38f3d2443c989c7399977c7b6
unknown <hayounn.etd@gmail.com>
2022-09-22 15:01:29 +0100

    Create methods for the posts class

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :likes, foreign_key: 'post_id'
   has_many :comments, foreign_key: 'post_id'
+  after_save :update_post_counter
+
+  scope :update_post_counter, ->(user_id) { User.find(user_id).update(post_counter: User.find(user_id).posts.count) }
+  scope :five_recent_comment, ->(post) { Comment.where(post_id: post).order(created_at: :desc).limit(3) }
+
+
 end
