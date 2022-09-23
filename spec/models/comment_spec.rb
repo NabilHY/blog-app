c22cd6841838cc81736b1a43c6bf63991fbb11d4
unknown <hayounn.etd@gmail.com>
2022-09-23 16:29:24 +0100

    Write unit tests for the Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Like, type: :model do
+RSpec.describe Comment, type: :model do
+
     subject do
         user = User.new(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', post_counter: 0)
         post = Post.new(author: user, title: 'Hello', text: 'This is my first post', likes_counter: 0, comment_counter: 0)
-        Like.new(author: user, post: post)
+        Comment.new(post: post, author: user, text: 'Hi Tom!')
     end
 
     before { subject.save }
@@ -13,9 +14,8 @@ RSpec.describe Like, type: :model do
         expect(subject).to be_valid
     end
 
-    it 'likes_count gets incremented by 1' do
-        subject.save
-        expect(subject.post.likes_counter).to eq(1)
+    it 'comment_counter gets incremented by 1' do
+        expect(subject.post.comment_counter).to eq(1)
     end
 
 end
