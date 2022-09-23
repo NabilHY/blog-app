3d78571547da3e416c6dde9d6dc88310c0f3778c
unknown <hayounn.etd@gmail.com>
2022-09-23 13:27:47 +0100

    Add blank validator for User name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
 
+  validates :name, presence: { strict: true }
   scope :recent_posts, ->(user_id) { Post.where(author_id: user_id).order(created_at: :desc).limit(3) }
 end
