276182bbc93699140a6ccdbdbf0b5274a6090c20
unknown <hayounn.etd@gmail.com>
2022-09-26 21:34:04 +0100

    Create Post controller and setup index page route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
+  resources :posts
   root 'users#index'
   # Defines the root path route ("/")
   # root "articles#index"
