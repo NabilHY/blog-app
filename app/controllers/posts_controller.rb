9b63f72dae0c48eb1ba5d33d1b17654a22daceb7
unknown <hayounn.etd@gmail.com>
2022-09-29 15:49:39 +0100

    Create Posts index wireframe using bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @posts = User.find(params[:id]).posts
-  end
+      @user = User.find(params[:user_id])
+      @posts = User.find(params[:user_id]).posts
+    end
 
   def show; end
 end
