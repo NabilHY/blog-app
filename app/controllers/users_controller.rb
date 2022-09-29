3d9025fdaa4645d2c1023b76a596b53be982043e
unknown <hayounn.etd@gmail.com>
2022-09-29 19:01:47 +0100

    Add links to users and posts routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = User.find(params[:id]).posts
-  end
+  end 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   scope :five_recent_comments, ->(post) { Comment.where(post_id: post).order(created_at: :desc).limit(5) }
-
+  
   validates :title, presence: { strict: true }, length: { minimum: 3, maximum: 250 }
   validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
