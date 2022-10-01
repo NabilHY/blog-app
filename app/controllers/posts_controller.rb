f2061258f73bfc1230f170aeace2d44b1b440340
unknown <hayounn.etd@gmail.com>
2022-10-01 14:37:00 +0100

    Refactor code to Add create action for comments records and link create comment from show page for posts records

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,14 +7,29 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
-    @comments = Post.five_recent_comments(params[:id])
+    @comments = @post.comments
   end
 
   def new
     post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
     respond_to do |format|
-      format.html { render :new, locals: { post: post } }
+      if @post.save
+        format.html { redirect_to user_posts_path(user_id: @post.author_id), notice: 'Friend was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create]
   end
+  resources :posts do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create]
+  end
 end
