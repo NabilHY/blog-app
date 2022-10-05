08b509e056ef358904f3922516389a5aca2cc5bf
unknown <hayounn.etd@gmail.com>
2022-10-05 22:12:23 +0100

    Refactor the code & Add a header partial to handle the devise logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :email, :password)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
   end
+  
 end
