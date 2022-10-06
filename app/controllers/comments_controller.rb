2edecbd4a5285cea133d08b9b854dad958a8f93b
unknown <hayounn.etd@gmail.com>
2022-10-06 19:34:06 +0100

    Make comment deletion functionnality available for auth users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
   def create
     @post = Post.find(params[:post_id])
-    @comment = current_user.comments.create!(text: comment_params, author_id: current_user.id, post_id: @post.id)
+    @comment = current_user.comments.new(comment_params)
     if @comment.save
       redirect_to user_post_path(@post.author_id, @post.id), notice: 'Comment was successfully created.'
     else
@@ -22,6 +23,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:text)[:text]
+    params.require(:comment).permit(:text).merge(post_id: params[:post_id])
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,7 @@ class Ability
       can :manage, :all
     else
       can :manage, Post, author_id: user.id
+      can :manage, Comment, author_id: user.id
       can :read, :all
     end
   end
